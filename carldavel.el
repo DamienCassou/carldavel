eb6b9f88be34cf1d7383c0dcb691b5ffe4d74247
Damien Cassou <damien@cassou.me>
2015-11-12 18:39:31 -0300

    Initial version (adapted from pycarddavel)

--- carldavel.el
+++ carldavel.el
@@ -4,6 +4,10 @@
 
 ;; Author: Damien Cassou <damien.cassou@inria.fr>
 ;; Version: 0.1
+;; GIT: https://github.com/DamienCassou/carldavel
+;; Package-Requires: ((helm "1.7.0") (emacs "24.0"))
+;; Created: 12 Nov 2015
+;; Keywords: helm pyccarddav vdirsyncer khard carddav caldav vdir contact addressbook calendar
 
 ;; This file is not part of GNU Emacs.
 
@@ -25,8 +29,144 @@
 ;; Integrate external tools to manage your contacts and calendars
 
 ;;; Code:
+
+(require 'eieio)
+(require 'helm)
 
+(defconst carldavel--buffer "*carldavel-contacts*")
 
+(defgroup carldavel nil
+  "Integrate external tools to manage your contacts and calendars"
+  :group 'external)
+
+(defcustom carldavel-contacts-fill-function
+  #'carldavel-khard-fill-buffer
+  "Function to get contacts from an external command."
+  :group 'carldavel
+  ;; TODO Improve the type by proposing existing commands
+  :type 'function)
+
+(defcustom carldavel-contacts-server-sync-function
+  #'carldavel-vdirsyncer-sync-server
+  "Function to sync local database with server."
+  :group 'carldavel
+  ;; TODO Improve the type by proposing existing commands
+  :type 'function)
+
+
+(defun carldavel--debug-info (string &rest objects)
+  "Log STRING with OBJECTS as if using `format`."
+  (apply #'message (concat "[carldavel] info: " string) objects))
+
+(defun carldavel--fill-buffer (buffer command &rest arguments)
+  "Fill BUFFER with contacts from COMMAND and ARGUMENTS.
+If BUFFER already contains something, erase it before executing COMMAND.
+Executing COMMAND with ARGUMENTS should print contacts in the mutt format
+to standard output.  The mutt format looks like:
+    some@email.com	Some Name
+    some2@mail.com	Some Other Name"
+  (carldavel--debug-info "Executing %s %s" command arguments)
+  (with-current-buffer buffer
+    (erase-buffer)
+    (apply
+     #'call-process
+     command
+     nil                                ; input file
+     (list buffer nil)                  ; output to buffer, discard error
+     nil                                ; don't redisplay
+     arguments)
+    ;; Remove first line as it is only informative
+    (goto-char (point-min))
+    (delete-region (point-min) (1+ (line-end-position)))))
+
+(defun carldavel-khard-fill-buffer (buffer)
+  "Call `carldavel--fill-buffer' on BUFFER.  Use khard."
+  (carldavel--fill-buffer buffer "khard" "mutt"))
+
+(defun carldavel-pycarddav-fill-buffer (buffer)
+  "Call `carldavel--fill-buffer' on BUFFER.  Use pc_query."
+  (carldavel--fill-buffer buffer "pc_query" "-m"))
+
+(defun carldavel--get-contacts-buffer ()
+  "Return a buffer with one carddav contact per line."
+  (let ((buffer (get-buffer-create carldavel--buffer)))
+    (when (with-current-buffer (get-buffer-create carldavel--buffer)
+            (equal (point-min) (point-max)))
+      (funcall carldavel-contacts-fill-function buffer))
+    buffer))
+
+(defun carldavel--reset-buffer ()
+  "Make sure carldavel buffer is empty to force update on next use."
+  (with-current-buffer (get-buffer-create carldavel--buffer)
+    (erase-buffer)))
+
+(defun carldavel--get-contact-from-line (line)
+  "Return a carddav contact read from LINE.
+
+The line must start with something like:
+some@email.com	Some Name
+
+The returned contact is of the form
+ (:name \"Some Name\" :mail \"some@email.com\")"
+  (when (string-match "\\(.*?\\)\t\\(.*?\\)\t" line)
+    (list :name (match-string 2 line) :mail (match-string 1 line))))
+
+(defun carldavel-sync-from-server (command &rest arguments)
+  "Use COMMAND with ARGUMENTS to sync local database with server."
+  (interactive)
+  (carldavel--debug-info "Executing %s %s" command arguments)
+  (apply
+   #'call-process
+   command
+   nil
+   (get-buffer-create "*carldavel-server-sync*")
+   nil
+   arguments))
+
+(defun carldavel-vdirsyncer-sync-server ()
+  "Ask vdirsyncer to sync with the server."
+  (carldavel-sync-from-server "vdirsyncer" "sync"))
+
+(defun carldavel-pycarddav-sync-server ()
+  "Ask pycardsyncer to sync contacts with the server."
+  (carldavel-sync-from-server "pycardsyncer"))
+
+(defun carldavel--helm-source-init ()
+  "Initialize helm candidate buffer."
+  (helm-candidate-buffer (carldavel--get-contacts-buffer)))
+
+(defun carldavel--helm-source-select-action (candidate)
+  "Print selected contacts as comma-separated text.
+CANDIDATE is ignored."
+  (ignore candidate)
+  (insert (mapconcat (lambda (contact)
+                       (let ((contact (carldavel--get-contact-from-line contact)))
+                         (format "\"%s\" <%s>"
+                                 (plist-get contact :name)
+                                 (plist-get contact :mail))))
+                     (helm-marked-candidates)
+                     ", ")))
+
+(defclass carldavel--helm-source (helm-source-in-buffer)
+  ((init :initform #'carldavel--helm-source-init)
+   (nohighlight :initform t)
+   (action :initform (helm-make-actions
+                      "Select" #'carldavel--helm-source-select-action))
+   (requires-pattern :initform 0)))
+
+;;;###autoload
+(defun carldavel-search-with-helm (refresh)
+  "Start helm to select your contacts from a list.
+If REFRESH is not-nil, make sure to ask pycarrdav to refresh the contacts
+list.  Otherwise, use the contacts previously fetched from pycarddav."
+  (interactive "P")
+  (when (and (consp refresh) (eq 16 (car refresh)))
+    (carldavel-sync-from-server))
+  (when refresh
+    (carldavel--reset-buffer))
+  (helm
+   :prompt "contacts: "
+   :sources (helm-make-source "Contacts" 'carldavel--helm-source)))
 
 (provide 'carldavel)
 
